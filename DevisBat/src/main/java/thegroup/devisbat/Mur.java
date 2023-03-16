dd6ef234885b045a667f645875e202f51381193f
fplumet01 <>
2023-03-16 12:15:34 +0100

    Le MUR fût ajouté

---
+++ DevisBat/src/main/java/thegroup/devisbat/Mur.java
@@ -0,0 +1,38 @@
+package thegroup.devisbat;
+
+public class Mur {
+    public Mur(int id, Coin c1, Coin c2)
+    {
+        this.id = id;
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+    private int id;
+    private Coin c1;
+    private Coin c2;
+
+    public Coin getC1() {
+        return c1;
+    }
+
+    public void setC1(Coin c1) {
+        this.c1 = c1;
+    }
+
+    public Coin getC2() {
+        return c2;
+    }
+
+    public void setC2(Coin c2) {
+        this.c2 = c2;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Mur{" + "id=" + id + ", c1=" + c1 + ", c2=" + c2 + '}';
+    }
+}
