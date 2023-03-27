6896e6cec15e26038ec24bdb891b986854d434d9
Feliplem <>
2023-03-27 17:00:15 +0200

    Appart fitness

--- DevisBat/src/main/java/thegroup/devisbat/Appartement.java
+++ DevisBat/src/main/java/thegroup/devisbat/Appartement.java
@@ -9,6 +9,68 @@ package thegroup.devisbat;
  * @author fplumet01
  */
 public class Appartement {
+
+    public Appartement(int id, Piece[] pieces) {
+        this.id = id;
+        this.pieces = pieces;
+    }
+    
     int id;
     Piece[] pieces;
+    String proprio;
+    
+    public double surface()
+    {
+        double s = 0;
+        for(Piece p : pieces)
+        {
+            s += p.surfacePiece();
+        }
+        return s;
+    }
+    
+    public void dessiner()
+    {
+        //
+    }
+    
+    public double devisAppartement()
+    {
+        double cout = 0;
+        for(Piece p : pieces){
+            cout += p.;
+        }
+        return cout;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Piece[] getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(Piece[] pieces) {
+        this.pieces = pieces;
+    }
+
+    public String getProprio() {
+        return proprio;
+    }
+
+    public void setProprio(String proprio) {
+        this.proprio = proprio;
+    }
 }
