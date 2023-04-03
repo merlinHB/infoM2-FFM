fe381725b8835ec389e5c63fa652fab7fb26e216
fscheer01 <fscheer01@sec-c212-f08.csip.in>
2023-04-03 16:56:11 +0200

    peinture v1

---
+++ DevisBat/src/main/java/thegroup/devisbat/Peinture.java
@@ -0,0 +1,17 @@
+package thegroup.devisbat;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fscheer01
+ */
+public class Peinture extends Revetement{
+    int idrevetement;
+    double surface;
+    double prix;
+    double cout;
+}
