adde69b3ba70414f6d706e91378f4ddfb7a16d6f
Feliplem <128042895+Feliplem@users.noreply.github.com>
2023-05-15 17:35:20 +0200

    Update Plafond.java

--- DevisBat/src/main/java/thegroup/devisbat/Plafond.java
+++ DevisBat/src/main/java/thegroup/devisbat/Plafond.java
@@ -1,4 +1,5 @@
 package thegroup.devisbat;
+package thegroup.devisbat;
 
 /**
  *
@@ -8,20 +9,17 @@ public class Plafond{
     
     private int id;
     private Piece piece;
-    private double surface;
     private Revetement revetement;
 
     public Plafond(int id, Piece piece, Revetement revetement) {
         this.piece = piece;
         this.revetement = revetement;
-        this.surface = piece.surfacePiece();
         this.id = id;
         this.piece.setPlafond(this);
     }
     public Plafond(int id, Piece piece, Revetement revetement, boolean save) {
         this.piece = piece;
         this.revetement = revetement;
-        this.surface = piece.surfacePiece();
         this.id = id;
         this.piece.setPlafond(this);
         if(save)
@@ -34,16 +32,15 @@ public class Plafond{
     public void setPiece(Piece p1)
     {
         this.piece = p1;
-        this.surface = p1.surfacePiece();
     }
     
     public double cout()
     {
-        return revetement.cout(surface);
+        return revetement.cout(surface());
     }
     
-    public void setSurface() {
-        this.surface = piece.surfacePiece();
+    public double surface() {
+        return piece.surfacePiece();
     }
     
     public int getId() {
